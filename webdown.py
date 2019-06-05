837c3a8514871110a2ee6e60db424ddca306624b
DaveKgw1 <45153558+davekgw@users.noreply.github.com>
2019-06-05 21:44:30 +0800

    Downweb

--- webdown.py
+++ webdown.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# python 3.3.2+ Hammer Dos Script v.1
-# by Can Yalçın
-# only for legal purpose
-
+# python 3.3.2+ Hammer DDos Script v.2
+# by Dave Kgw
 
 from queue import Queue
 from optparse import OptionParser
